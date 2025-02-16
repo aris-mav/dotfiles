32b89f98c3db0026204b48704d1883df1041e1bb
arismav <aris.mav@hotmail.com>
2025-02-16 16:00:57 +0000

    added entr macro

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -2,3 +2,18 @@ if isinteractive()
     @eval using VimBindings
     @eval using Revise
 end
+
+
+"""
+    @entr expression
+Whenever a .jl file is saved, expr will be evaluated.
+"""
+macro entr(x)
+
+    return :(
+        Revise.entr([".jl"]) do
+            $x
+        end
+    )
+
+end
