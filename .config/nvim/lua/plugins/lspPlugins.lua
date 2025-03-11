f61f2ebb4a72f57d6304c06be053fe85e04bf47a
aris <aris.mav@hotmail.com>
2025-03-11 15:03:24 +0000

    nvim updated cmp autocompletion

--- .config/nvim/lua/plugins/lspPlugins.lua
+++ .config/nvim/lua/plugins/lspPlugins.lua
@@ -10,17 +10,27 @@ return {
     {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer',
+        },
         config = function()
             local cmp = require('cmp')
 
             cmp.setup({
                 sources = {
                     {name = 'nvim_lsp'},
+                    { name = 'path' },  -- Enables filename/path completion
+                    { name = 'buffer' },
                 },
                 mapping = cmp.mapping.preset.insert({
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<Tab>'] = cmp.mapping.select_next_item(),
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
                 }),
                 snippet = {
                     expand = function(args)
