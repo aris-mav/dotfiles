6ffb3a2f9ea8e691b561831386c6c1b14d86b206
arismav <aris.mav@hotmail.com>
2025-03-22 09:55:05 +0000

    julia unicode in markdown

--- .config/nvim/lua/plugins/juliavim.lua
+++ .config/nvim/lua/plugins/juliavim.lua
@@ -1,3 +1,6 @@
 return {
-    'JuliaEditorSupport/julia-vim'
-    }
+    'JuliaEditorSupport/julia-vim',
+    config = function()
+        vim.g.latex_to_unicode_file_types = {'julia', 'markdown' }
+    end,
+}
