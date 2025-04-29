4357fda03c25c0f74013a41318162854d3e31e9d
aris <aris.mav@hotmail.com>
2025-04-29 11:19:01 +0100

    added gr and gR

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 # Set up PATH entries
-fish_add_path /home/arismav/.local/bin
-fish_add_path /home/arismav/.scripts
-fish_add_path /opt/nvim-linux-x86_64/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.scripts
 fish_add_path $HOME/.juliaup/bin/
+fish_add_path /opt/nvim-linux-x86_64/bin
 
 if string match -q '*com.termux*' "$PREFIX"
     fish_add_path /data/data/com.termux/files/home/.cargo/bin
@@ -18,6 +18,8 @@ abbr jl 'julia -t auto'
 abbr z 'zellij'
 abbr gC 'git commit'
 abbr gc 'git checkout'
+abbr gr 'git restore'
+abbr gR 'git reset'
 abbr gf 'git fetch'
 abbr ga 'git add'
 abbr gd 'git diff'
@@ -28,6 +30,7 @@ abbr gb 'git branch'
 abbr gacp 'git add . ; git commit ; git push '
 abbr fp '$FUZZYFIND --preview="less {}"'
 
+# Define some software preferences
 for candidate in nvim hx vim vi
     if type -q $candidate
         set -gx EDITOR $candidate
@@ -42,19 +45,21 @@ for candidate in sk fzf
     end
 end
 
-
+# For interactive sessions
 if status is-interactive
 
+    # vi mode settings
     fish_vi_key_bindings
-    # Set the cursor shapes for the different vi modes.
     set fish_cursor_default     block      
     set fish_cursor_insert      line       blink
     set fish_cursor_replace_one underscore blink
     set fish_cursor_visual      bloch
 
+    # zellij auto-start
     if type -q zellij
         set -gx ZELLIJ_AUTO_ATTACH true
         eval (zellij setup --generate-auto-start fish | string collect)
     end
+
 end
 
--- .zshrc
+++ .zshrc
@@ -154,3 +154,5 @@ if [[ "$PREFIX" == *"com.termux"* ]]; then
 else
     source /home/arismav/.config/broot/launcher/bash/br
 fi 
+
+source /home/arismav/.config/broot/launcher/bash/br
