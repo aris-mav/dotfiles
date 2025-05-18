fe322246b94750ab5cac79cb37469aa2f03ee4cf
arismav <aris.mav@hotmail.com>
2025-05-18 11:29:46 +0100

    switched to nontransparent and fixed bugs

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,32 +1,37 @@
 return {
 	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
-	config = true,
-	opts = {
-
-		terminal_colors = true, -- add neovim terminal colors
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = {
-			strings = false,
-			emphasis = false,
-			comments = true,
-			operators = false,
-			folds = true,
-		},
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		invert_intend_guides = false,
-		inverse = true, -- invert background for search, diffs, statuslines and errors
-		contrast = "hard", -- can be "hard", "soft" or empty string
-		palette_overrides = {},
-		overrides = {},
-		dim_inactive = false,
-		transparent_mode = true,
-	}
-
+    config = function()
+        require("gruvbox").setup({
+            transparent_mode = false,
+            terminal_colors = true, -- add neovim terminal colors
+            undercurl = true,
+            underline = true,
+            bold = true,
+            italic = {
+                strings = false,
+                emphasis = false,
+                comments = true,
+                operators = false,
+                folds = true,
+            },
+            strikethrough = true,
+            invert_selection = false,
+            invert_signs = false,
+            invert_tabline = false,
+            invert_intend_guides = false,
+            inverse = true, -- invert background for search, diffs, statuslines and errors
+            -- contrast = "hard", -- can be "hard", "soft" or empty string
+            palette_overrides = {},
+            dim_inactive = false,
+            overrides = {
+                SignColumn = { bg = "none" },
+                DiagnosticSignError = { fg = "#fb4934", bg = "none" },
+                DiagnosticSignWarn  = { fg = "#fabd2f", bg = "none" },
+                DiagnosticSignInfo  = { fg = "#83a598", bg = "none" },
+                DiagnosticSignHint  = { fg = "#8ec07c", bg = "none" },
+            }
+        })
+        vim.cmd.colorscheme("gruvbox")
+    end,
 }
-
