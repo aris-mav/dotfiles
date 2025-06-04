1bb53b636318aab57d674cfa83c292a00283a080
arismav <aris.mav@hotmail.com>
2025-06-04 18:57:53 +0100

    check for binaries

--- .config/fish/functions/ytd.fish
+++ .config/fish/functions/ytd.fish
@@ -1,5 +1,13 @@
 function ytd
 
+    if not type -q yt-dlp
+        echo "oi, you need to install yt-dlp"
+        return 1
+    else if not type -q eyeD3
+        echo "oi, you need to install eyeD3"
+        return 1
+    end
+
     read -l -P  "YouTube url : " url
     read -l -P  "file name : " filename
     read -l -P  "Song name : " songname
@@ -7,6 +15,6 @@ function ytd
     read -l -P  "Album : " album
 
     yt-dlp -x --audio-format mp3 --audio-quality 0 -o $filename $url
-    eyeD3 -a $artist -t $songname -A $artist $filename
+    eyeD3 -a $artist -t $songname -A $album $filename
 
 end
