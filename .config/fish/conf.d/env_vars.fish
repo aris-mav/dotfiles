a32b6277346dc6a283ea50b80293a959b160b596
arismav <aris.mav@hotmail.com>
2025-06-20 19:19:51 +0100

    added manpager nvim

--- .config/fish/conf.d/env_vars.fish
+++ .config/fish/conf.d/env_vars.fish
@@ -10,12 +10,17 @@ end
 for candidate in EDITOR FUZZYFIND FILE_BROWSER
     if not set -q {$candidate}
         switch $candidate
-            case EDITOR
-                set -Ux EDITOR (choose_first_available nvim vim vi hx)
-            case FUZZYFIND
-                set -Ux FUZZYFIND (choose_first_available sk fzf)
-            case FILE_BROWSER
-                set -Ux FILE_BROWSER (choose_first_available br yazi ranger)
+        case EDITOR
+            set -Ux EDITOR (choose_first_available nvim vim vi hx)
+
+            if test $EDITOR = nvim 
+                set -Ux MANPAGER 'nvim +Man!'
+            end
+
+        case FUZZYFIND
+            set -Ux FUZZYFIND (choose_first_available sk fzf)
+        case FILE_BROWSER
+            set -Ux FILE_BROWSER (choose_first_available br yazi ranger)
         end
     end
 end
