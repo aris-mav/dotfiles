9d926e4d13b2cb1fb3b425809a80e4b241fafd77
arismav <aris.mav@hotmail.com>
2025-07-25 22:02:51 +0100

    added mason autoinstall

--- .config/nvim/lua/plugins/lspPlugins.lua
+++ .config/nvim/lua/plugins/lspPlugins.lua
@@ -52,7 +52,12 @@ return {
             })
 
             require('mason-lspconfig').setup({
-                ensure_installed = {},
+                ensure_installed = {
+                    "lua_ls",
+                    "texlab",
+                    "rust_analyzer",
+                    "markdown_oxide"
+                },
                 handlers = {
                     -- this first function is the "default handler"
                     -- it applies to every language server without a "custom handler"
