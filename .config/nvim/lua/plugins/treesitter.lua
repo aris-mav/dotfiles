9e4d67ad3dbaad06a58391773b58ff605151291e
arismav <aris.mav@hotmail.com>
2025-07-28 21:59:05 +0100

    update ensure installed section

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -16,7 +16,19 @@ return {
 
         require'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all"
-            ensure_installed = { "lua", "python", "markdown", "julia", "rust", },
+            ensure_installed = {
+                "lua",
+                "python",
+                "markdown",
+                "markdown_inline",
+                "julia",
+                "rust",
+                "fish",
+                "vim",
+                "tmux",
+                "toml",
+                "gitcommit",
+            },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
