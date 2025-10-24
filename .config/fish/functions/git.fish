5a8b653bb51bc03829f8d814794e50978551a677
arismav <aris.mav@hotmail.com>
2025-10-24 08:37:34 +0200

    modified the echo message

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -2,7 +2,7 @@ function git
     # If we're in $HOME
     if test (pwd) = $HOME
         # Use your bare dotfiles repo
-        echo "Using dotfiles bare repo fish function instead of normal git command."
+        echo "Using git on dotfiles bare repo."
         command git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME $argv
     else
         # Normal git behavior elsewhere
