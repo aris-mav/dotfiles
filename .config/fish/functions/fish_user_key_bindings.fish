cdbfafa0ec678174095087f734a8dfe64c6d447a
arismav <aris.mav@hotmail.com>
2025-11-23 13:52:01 +0100

    ctrl f to file browser

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,11 @@
 function fish_user_key_bindings
 
     # Ctrl-Z to fg
-    bind -Mdefault -Minsert \cZ 'fg 2>/dev/null ; commandline -f repaint ; fish_vi_key_bindings insert'
+    bind -Minsert \cZ 'fg 2>/dev/null ; commandline -f repaint ; fish_vi_key_bindings insert'
+    bind -Mdefault \cZ 'fg 2>/dev/null ; commandline -f repaint ; fish_vi_key_bindings default'
+
+    # Ctrl-f to file browser
+    bind -Minsert \cF "$FILE_BROWSER ; commandline -f repaint ; fish_vi_key_bindings insert"
+    bind -Mdefault \cF "$FILE_BROWSER ; commandline -f repaint ; fish_vi_key_bindings default"
 
 end
