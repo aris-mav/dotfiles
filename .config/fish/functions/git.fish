ac96907963ea6495c6fc4425155de16d3a65fb32
arismav <aris.mav@hotmail.com>
2025-12-08 13:47:38 +0100

    remove echo string

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -2,7 +2,6 @@ function git
     # If we're in $HOME
     if test (pwd) = $HOME
         # Use your bare dotfiles repo
-        echo "Using git on dotfiles bare repo."
         command git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME $argv
     else
         # Normal git behavior elsewhere
