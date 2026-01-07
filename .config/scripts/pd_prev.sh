66687fbf233953a51ec360ccfae4945c5088278f
arismav <aris.mav@hotmail.com>
2026-01-07 15:11:07 +0100

    bugfix

--- .config/scripts/pd_prev.sh
+++ .config/scripts/pd_prev.sh
@@ -6,7 +6,7 @@ id=$(cksum "$input_markdown_file" | awk '{print $1}')
 tmpfilename="/tmp/note$id.pdf"
 
 if [ ! -f "$tmpfilename" ]; then
-  pandoc "$file" -o "$tmpfilename"
+  pandoc "$input_markdown_file" -o "$tmpfilename"
 fi
 
 xdg-open "$tmpfilename" >/dev/null 2>&1 &
