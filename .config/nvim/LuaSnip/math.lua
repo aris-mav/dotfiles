9a0388225cdc17f9befdf3bbaee72d1f28b8abcf
arismav <aris.mav@hotmail.com>
2026-01-12 23:03:16 +0100

    frac and mathrm

--- .config/nvim/LuaSnip/math.lua
+++ .config/nvim/LuaSnip/math.lua
@@ -1,3 +1,29 @@
 return { -- these are meant to be shared between tex and md files
 
+    s(
+        {
+            trig="upright",
+            dscr="Latex mathrm for non-italic symbols in equations",
+        },
+        {
+            t("\\mathrm{"),
+            i(1, "symbol"),
+            t("}"),
+        }
+    ),
+
+    s(
+        {
+            trig="frac",
+            descr="Latex fraction"
+        },
+        {
+            t("\\frac{"),
+            i(1, "numerator"),
+            t("}{"),
+            i(2, "denominator"),
+            t("}"),
+        }
+    )
+
 }
