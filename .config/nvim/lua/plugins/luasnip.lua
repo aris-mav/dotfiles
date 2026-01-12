3e0989731eedc7eb7eb0f6f08bcccfeb28b44a74
arismav <aris.mav@hotmail.com>
2026-01-12 21:53:28 +0100

    working version for luasnip-cmp

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -6,8 +6,38 @@ return {
 	build = "make install_jsregexp",
 
 	config = function()
+
 		local ls = require("luasnip")
 		local cmp = require("cmp")
+
+        require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
+
+        vim.cmd[[ 
+        " Use Tab to expand and jump through snippets
+        imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+        smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+        " Use Shift-Tab to jump backwards through snippets
+        imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+        smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>' ]]
+
+        -- also define another expand keybind, in case cmp is triggered as well
+        vim.keymap.set({"i"}, "<C-L>", function()
+            ls.expand()
+        end, {silent = true})
+
+        vim.keymap.set({"i", "s"}, "<C-E>", function()
+            if ls.choice_active() then
+                ls.change_choice(1)
+            end
+        end, {silent = true})
+
+        ls.config.set_config({ -- Setting LuaSnip config
+            -- Enable autotriggered snippets
+            enable_autosnippets = true,
+            -- Use Tab (or some other key if you prefer) to trigger visual selection
+            store_selection_keys = "<Tab>",
+        })
+
 
 		cmp.setup({
 			-- ... Your other configuration ...
@@ -47,8 +77,9 @@ return {
 				end, { "i", "s" }),
 
 				-- ... Your other mappings ...
-			},
-			-- ... Your other configuration ...
-		})
-	end,
+
+            },
+            -- ... Your other configuration ...
+        })
+    end,
 }
