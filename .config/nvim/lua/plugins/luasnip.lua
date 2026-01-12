8ca6aac2ce0029f4730e90f6d9a46aea11d5a095
arismav <aris.mav@hotmail.com>
2026-01-12 22:47:44 +0100

    added snippet undo, just in case

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,14 +1,14 @@
 return {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp",
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
 
-	config = function()
+    config = function()
 
-		local ls = require("luasnip")
-		local cmp = require("cmp")
+        local ls = require("luasnip")
+        local cmp = require("cmp")
 
         require("luasnip").filetype_extend("tex", { "math" })
         require("luasnip").filetype_extend("markdown", { "math" })
@@ -41,45 +41,71 @@ return {
             store_selection_keys = "<Tab>",
         })
 
+        local untrigger = function() --courtesy of https://github.com/L3MON4D3/LuaSnip/issues/797#issuecomment-1970013181
+            -- get the snippet
+            local snip = require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()].parent.snippet
+            -- get its trigger
+            local trig = snip.trigger
+            -- replace that region with the trigger
+            local node_from, node_to = snip.mark:pos_begin_end_raw()
+            vim.api.nvim_buf_set_text(
+                0,
+                node_from[1],
+                node_from[2],
+                node_to[1],
+                node_to[2],
+                { trig }
+            )
+            -- reset the cursor-position to ahead the trigger
+            vim.fn.setpos(".", { 0, node_from[1] + 1, node_from[2] + 1 + string.len(trig) })
+        end
 
-		cmp.setup({
-			-- ... Your other configuration ...
-			mapping = {
-				['<CR>'] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						if ls.expandable() then
-							ls.expand()
-						else
-							cmp.confirm({
-								select = true,
-							})
-						end
-					else
-						fallback()
-					end
-				end),
+        vim.keymap.set({ "i", "s" }, "<c-z>", function()
+            if require("luasnip").in_snippet() then
+                untrigger()
+                require("luasnip").unlink_current()
+            end
+        end, { desc = "Undo a snippet", })
 
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif ls.locally_jumpable(1) then
-						ls.jump(1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
 
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif ls.locally_jumpable(-1) then
-						ls.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+        cmp.setup({
+            -- ... Your other configuration ...
+            mapping = {
+                ['<CR>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        if ls.expandable() then
+                            ls.expand()
+                        else
+                            cmp.confirm({
+                                select = true,
+                            })
+                        end
+                    else
+                        fallback()
+                    end
+                end),
 
-				-- ... Your other mappings ...
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif ls.locally_jumpable(1) then
+                        ls.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif ls.locally_jumpable(-1) then
+                        ls.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                -- ... Your other mappings ...
 
             },
             -- ... Your other configuration ...
