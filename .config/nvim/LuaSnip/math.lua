133cb2562ab1d2e2c92c7bb745a0c9d64ec9b7b4
arismav <aris.mav@hotmail.com>
2026-01-13 23:10:32 +0100

    added derivative and integral

--- .config/nvim/LuaSnip/math.lua
+++ .config/nvim/LuaSnip/math.lua
@@ -1,3 +1,14 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
 return { -- these are meant to be shared between tex and md files
 
     s(
@@ -24,6 +35,37 @@ return { -- these are meant to be shared between tex and md files
             i(2, "denominator"),
             t("}"),
         }
-    )
+    ),
 
+    s(
+        {
+            trig="derivative",
+            descr="Leibnitz notation for 1st monovariate derivative"
+        },
+        {
+            t("\\frac{\\mathrm{d} "),
+            i(1, "f"),
+            t(" }{\\mathrm{d} "),
+            i(2, "x"),
+            t(" }"),
+        }
+    ),
+
+    s(
+        {
+            trig="integral",
+            descr="Bounded integral"
+        },
+        {
+            t("\\int_{"),
+            i(1, ""),
+            t("}^{"),
+            i(2, ""),
+            t("}{"),
+            i(3, ""),
+            t("}{\\mathrm{d}"),
+            i(4, ""),
+            t("}")
+        }
+    ),
 }
