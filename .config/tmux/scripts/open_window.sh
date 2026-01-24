dd6d83e31c06d146379105728397c131257dee40
arismav <aris.mav@hotmail.com>
2026-01-24 10:46:01 +0100

    open window script updates

--- .config/tmux/scripts/open_window.sh
+++ .config/tmux/scripts/open_window.sh
@@ -3,21 +3,43 @@
 WIN="$1"
 CMD="$2"
 
-if [ -z "$WIN" ] || [ -z "$CMD" ]; then
+if [ -z "$WIN" ]; then
     echo "Usage: $0 <window-name> <command>"
     exit 1
 fi
 
-# Check if the window exists
-if tmux list-windows -F '#W' | grep -qx "$WIN"; then
+case "$WIN" in
+    [0-9])
+        current_win=$(tmux display -p "#W")
+        if [ "$current_win" = "$WIN" ]; then
+            # go to previous window
+            tmux select-window -l 
+        else
+            # go to chosen window
+            tmux select-window -t :"$WIN" || \
+                tmux display-message "Can't find window: $WIN"
+        fi
+        exit 0
+        ;;
 
-    current_win=$(tmux display -p "#W")
-    if [ $current_win = $WIN ] ; then
-        tmux select-window -l
-    else
-        tmux select-window -t :"$WIN"
-    fi
-
-else
-    tmux new-window -n "$WIN" "$CMD"
-fi
+    *) 
+        # Check if the window exists
+        if tmux list-windows -F '#W' | grep -qx "$WIN"; then
+            current_win=$(tmux display -p "#W")
+            if [ "$current_win" = "$WIN" ]; then
+                tmux select-window -l 
+            else
+                tmux select-window -t :"$WIN"
+            fi
+            exit 0
+        else 
+            # if the window does not already exist
+            if [ -z "$CMD" ]; then
+                tmux new-window -n "$WIN"
+            else
+                tmux new-window -n "$WIN" "$CMD"
+            fi
+            exit 0
+        fi
+        ;;
+esac
