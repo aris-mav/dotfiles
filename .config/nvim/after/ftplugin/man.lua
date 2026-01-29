8fba8e406ec33a7bd77f5da485565cd4fbd4199b
arismav <aris.mav@hotmail.com>
2026-01-29 22:58:21 +0100

    fix keybind

--- .config/nvim/after/ftplugin/man.lua
+++ .config/nvim/after/ftplugin/man.lua
@@ -1,6 +1,6 @@
 -- man pager options
+vim.keymap.set("n", "f", "<C-f>", { remap = true, silent = false })
+vim.keymap.set("n", "b", "<C-b>", { remap = true, silent = false })
 vim.keymap.set("n", "d", "<C-d>zzzv", { remap = true, silent = false })
 vim.keymap.set("n", "u", "<C-u>zzzv", { remap = true, silent = false })
-vim.keymap.set("n", "f", "<C-f>zzzv", { remap = true, silent = false })
-vim.keymap.set("n", "b", "<C-b>zzzv", { remap = true, silent = false })
 vim.keymap.set("n", "q", ":q<CR>", { buffer = true, silent = true })
