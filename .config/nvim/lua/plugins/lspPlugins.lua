cf0a7cde6adbe49bf872ac54f83a7b53256084d7
arismav <aris.mav@hotmail.com>
2026-01-30 16:41:37 +0100

    change lsp and TS incremental selection keymaps

--- .config/nvim/lua/plugins/lspPlugins.lua
+++ .config/nvim/lua/plugins/lspPlugins.lua
@@ -36,18 +36,19 @@ return {
             vim.api.nvim_create_autocmd('LspAttach', {
                 desc = 'LSP actions',
                 callback = function(event)
-                    local opts = {buffer = event.buf}
+                    local opts = { buffer = event.buf }
 
-                    vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references)
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-                    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
-                    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                    vim.keymap.set("n", "K",          vim.lsp.buf.hover,           opts)
+                    vim.keymap.set("n", "gd",         vim.lsp.buf.definition,      opts)
+                    vim.keymap.set("n", "gD",         vim.lsp.buf.declaration,     opts)
+                    vim.keymap.set("n", "gi",         vim.lsp.buf.implementation,  opts)
+                    vim.keymap.set("n", "go",         vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set("n", "gr",         vim.lsp.buf.references,      opts)
+                    vim.keymap.set("n", "gs",         vim.lsp.buf.signature_help,  opts)
+                    vim.keymap.set("n", "<C-s>",      vim.lsp.buf.signature_help,  opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,          opts)
+                    vim.keymap.set("n", "<leader>d",  vim.diagnostic.open_float,   opts)
+
                 end,
             })
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -67,10 +67,10 @@ return {
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = "<C-s>",
-                    node_incremental = "<C-s>",
-                    scope_incremental = false,
-                    node_decremental = "<BS>",
+                    init_selection = "+",
+                    node_incremental = "+",
+                    scope_incremental = "=",
+                    node_decremental = "-",
                 },
             },
 
