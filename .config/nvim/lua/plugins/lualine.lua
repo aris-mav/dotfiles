dd2158939bddf043ef8a34cb5d707118b0b01d54
arismav <aris.mav@hotmail.com>
2026-02-06 15:58:30 +0100

    show lsp status

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,8 +8,9 @@ return {
             options = {
                 -- theme = 'gruvbox',
                 icons_enabled = true,
-                component_separators = { left = '', right = ''},
+                -- component_separators = { left = '', right = ''},
                 section_separators = { left = '', right = ''},
+                component_separators = { left = '', right = ''},
                 disabled_filetypes = {
                     statusline = {},
                     winbar = {},
@@ -25,11 +26,60 @@ return {
                 }
             },
             sections = {
+
                 lualine_a = {'branch'},
                 lualine_b = {'diff', 'diagnostics'},
-                lualine_c = {'filename'},
+                lualine_c = {
+                    {
+                        'filename',
+                        file_status = true,      -- Displays file status (readonly status, modified status)
+                        newfile_status = false,  -- Display new file status (new file means no write after created)
+                        path = 1,
+                        -- 0: Just the filename
+                        -- 1: Relative path
+                        -- 2: Absolute path
+                        -- 3: Absolute path, with tilde as the home directory
+                        -- 4: Filename and parent dir, with tilde as the home directory
+
+                        shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                        -- for other components. (terrible name, any suggestions?)
+                        -- It can also be a function that returns
+                        -- the value of `shorting_target` dynamically.
+                        symbols = {
+                            modified = '[+]',      -- Text to show when the file is modified.
+                            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                            unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                            newfile = '[New]',     -- Text to show for newly created file before first write
+                        }
+                    }
+                },
+
                 lualine_x = {'encoding'},
-                lualine_y = {'filetype'},
+                lualine_y = {
+                    {
+                        'lsp_status',
+                        -- icon = '',
+                        icon = false,
+                        symbols = {
+                            -- Standard unicode symbols to cycle through for LSP progress:
+                            spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+                            -- Standard unicode symbol for when LSP is done:
+                            done =  '',
+                            -- Delimiter inserted between LSP names:
+                            separator = '',
+                        },
+                        -- List of LSP names to ignore (e.g., `null-ls`):
+                        ignore_lsp = {},
+                        -- Display the LSP name
+                        show_name = false,
+                    },
+                    {
+                        'filetype',
+                        colored = true,   -- Displays filetype icon in color if set to true
+                        icon_only = true, -- Display only an icon for filetype
+                        icon = { align = 'left' }, -- Display filetype icon on the right hand side
+                    },
+                },
                 lualine_z = {'location'}
             },
             inactive_sections = {
