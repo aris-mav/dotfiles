0057dbb91ba30fa4037331852a2ea5b10140e1bb
arismav <aris.mav@hotmail.com>
2026-04-07 16:20:41 +0200

    slightly increase main pane dimensions

--- .config/tmux/scripts/default_split.sh
+++ .config/tmux/scripts/default_split.sh
@@ -4,7 +4,7 @@ width=$(tmux display -p "#{pane_width}");
 
 if [ "$width" -gt 150 ]; then 
     tmux split-window -h; 
-    tmux resize-pane -x $((width / 2 + 25)); 
+    tmux resize-pane -x $((width / 2 + 28)); 
 elif [ "$width" -gt 120 ]; then 
     tmux split-window -h; 
     tmux resize-pane -x $((width * 66 / 100)); 
