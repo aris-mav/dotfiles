a3fe5cc78a449e8959a22491fbc4d73d6c7283aa
arismav <aris.mav@hotmail.com>
2026-04-23 21:02:48 +0200

    cleanup

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,4 +1,3 @@
-    # git stuff
     abbr gC 'git commit'
     abbr gc 'git checkout'
     abbr gr 'git restore'
@@ -18,33 +17,37 @@
     abbr gau 'git add -u'
     abbr gacp 'git add -u ; git commit ; git push '
 
-    # general stuff
+    if set -q FUZZYFIND
+
+        abbr gaf "git add (git diff --name-only \
+        | $FUZZYFIND --multi --preview 'git diff --color=always {}')"
+
+        abbr grf "git reset (git diff --name-only --cached \
+        | $FUZZYFIND --multi --preview 'git diff --color=always --cached {}')"
+
+        abbr gdf "git diff --name-only \
+        | $FUZZYFIND --multi --preview 'git diff --color=always {}'"
+
+        abbr cdp "cd (tmux list-panes -a -F '#{pane_current_path}' | $FUZZYFIND)"
+
+    end
+
     abbr e "$EDITOR"
     abbr f "$FILE_BROWSER"
     abbr y "$YANKTEXT"
     abbr xo 'xdg-open'
     abbr x 'xargs'
 
-    # nt 
     abbr nn "nt -n"
     abbr ns "nt -s"
-
-    if type -q tmux
-        abbr cdp 'cd (tmux list-panes -a -F "#{pane_current_path}" | fzf)'
-    end
 
     if type -q pacman
         abbr pa 'sudo pacman'
     else if type -q apt
         abbr pa 'sudo apt'
     else if type -q nix
-        abbr rb 'sudo nixos-rebuild switch -I nixos-config=~/.config/nixos/configuration.nix'
+        abbr rb 'sudo nixos-rebuild switch \
+        -I nixos-config=~/.config/nixos/configuration.nix'
         abbr ns 'nix-shell --command fish -p '
     end
 
-    if set -q FUZZYFIND
-
-        abbr fp "$FUZZYFIND --preview='less {}'"
-        abbr gaf "git add (git diff --name-only | $FUZZYFIND --multi --preview 'git diff --color=always {}')"
-
-    end
