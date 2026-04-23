ec930dea2ec19f98c25d2f92f5c43c5787f5aad1
arismav <aris.mav@hotmail.com>
2026-04-23 09:51:00 +0200

    rm fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,12 @@
 # For interactive sessions:
 if status is-interactive
 
-    # vi mode settings
     fish_vi_key_bindings
     set fish_cursor_default     block      
     set fish_cursor_insert      line       blink
     set fish_cursor_replace_one underscore blink
     set fish_cursor_visual      block
 
-    # disables the greeting
     set fish_greeting 
-
-    if type -q fzf
-        fzf --fish | source
-    end
 
 end
