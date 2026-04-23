8675bdb9bd601b78f9bf37d526a075e6c262a62f
arismav <aris.mav@hotmail.com>
2026-04-23 19:23:56 +0200

    improve logic

--- .config/tmux/scripts/auto_split.sh
+++ .config/tmux/scripts/auto_split.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 
-tmux_info=$(
+info=$(
     tmux display-message -p \
         "#{window_panes} #{pane_width} #{pane_height} #{pane_current_path}"
-)
+    )
 
-read -r panes width height path << EOF
-$tmux_info
-EOF
+# shellcheck disable=SC2086
+set -- $info
 
-if [ "$panes" -eq 1 ]; then
+panes=$1
+width=$2
+height=$3
+path=$4
+
+if [ "$panes" -eq 1 ] && [ "$width" -gt 120 ]; then
 
     if [ "$width" -gt 150 ]; then 
-        tmux split-window -b -h; 
-        tmux resize-pane -x $((width / 2 - 28)); 
-    elif [ "$width" -gt 120 ]; then 
-        tmux split-window -b -h; 
-        tmux resize-pane -x $((width * 33 / 100)); 
+        size="$((width / 2 - 28))"
+    else
+        size="$((width * 33 / 100))"
     fi
+    tmux split-window -d -b -h -l $size
 
-elif [ "$((width / height))" -gt 3 ]; then
-    tmux split-window -h -c "$path"
-else
-    tmux split-window -v -c "$path"
+elif [ "$width" -gt 80 ] && [ "$height" -gt 30 ]; then
+
+    tmux split-window -v -c "$path" -l "$((height * 33 / 100))"
+
+elif [ "$width" -ge 30 ] && [ "$height" -ge 10 ]; then
+
+    [ "$((width / height))" -gt 3 ] \
+        && split_type="-h" \
+        || split_type="-v"
+
+    tmux split-window "$split_type" -c "$path"
 fi
