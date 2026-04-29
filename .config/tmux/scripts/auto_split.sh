db25792cd0ee53afe35ef5e8e782e6a9e80005a1
arismav <aris.mav@hotmail.com>
2026-04-29 21:49:42 +0200

    simplify numbers

--- .config/tmux/scripts/auto_split.sh
+++ .config/tmux/scripts/auto_split.sh
@@ -18,13 +18,13 @@ if [ "$panes" -eq 1 ] && [ "$width" -gt 120 ]; then
     if [ "$width" -gt 150 ]; then 
         size="$((width / 2 - 28))"
     else
-        size="$((width * 33 / 100))"
+        size="$((width / 3))"
     fi
     tmux split-window -d -b -h -l $size -c "$path"
 
 elif [ "$width" -gt 80 ] && [ "$height" -gt 30 ]; then
 
-    tmux split-window -v -c "$path" -l "$((height * 33 / 100))"
+    tmux split-window -v -c "$path" -l "$((height / 3))"
 
 elif [ "$width" -ge 30 ] && [ "$height" -ge 10 ]; then
 
--- .config/tmux/scripts/default_split.sh
+++ .config/tmux/scripts/default_split.sh
@@ -8,5 +8,5 @@ if [ "$width" -gt 150 ]; then
 
 elif [ "$width" -gt 120 ]; then 
 
-    tmux split-window -d -b -h -l "$((width * 33 / 100))"
+    tmux split-window -d -b -h -l "$((width / 3))"
 fi
