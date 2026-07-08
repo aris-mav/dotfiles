a669a06c115061c14a77c8240013e981a796c734
arismav <aris.mav@hotmail.com>
2026-07-08 10:18:52 +0200

    move keybinds

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -109,3 +109,26 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.keymap.set("n", "cp", function()
     vim.fn.setreg("+", vim.fn.expand("%"))
 end, { noremap = true, silent = true })
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local opts = { buffer = event.buf }
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>=', function()
+            vim.lsp.buf.format({ async = true })
+        end, opts)
+    end,
+})
--- .config/nvim/lua/plugins/lspPlugins.lua
+++ .config/nvim/lua/plugins/lspPlugins.lua
@@ -31,30 +31,6 @@ return {
                 require('cmp_nvim_lsp').default_capabilities()
             )
 
-            -- LspAttach is where you enable features that only work
-            -- if there is a language server active in the file
-            vim.api.nvim_create_autocmd('LspAttach', {
-                desc = 'LSP actions',
-                callback = function(event)
-                    local opts = { buffer = event.buf }
-
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                    vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                    vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-                    vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<leader>=', function()
-                        vim.lsp.buf.format({ async = true })
-                    end, opts)
-                end,
-            })
-
             -- Detect Termux
             local is_termux = os.getenv("PREFIX") and os.getenv("PREFIX"):match("com.termux")
 
