5166208bfb5cef148ec1213608a8c206fe30fb54
arismav <aris.mav@hotmail.com>
2026-07-15 09:18:48 +0200

    update link and remove ^

--- .config/nvim/LuaSnip/markdown.lua
+++ .config/nvim/LuaSnip/markdown.lua
@@ -1,8 +1,9 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
-local f = ls.function_node
 local i = ls.insert_node
+local d = ls.dynamic_node
+local sn = ls.snippet_node
 
 local yaml_header = s({},
     {
@@ -45,16 +46,22 @@ return {
         t("["),
         i(1, "text"),
         t("]("),
-        f(function()
-            return vim.fn.getreg('+'):gsub("%s+", "")
-        end),
+        d(2, function()
+            local clipboard = vim.fn.getreg('+'):gsub("%s+", "")
+            if clipboard == "" then
+                clipboard = "url"
+            end
+            return sn(nil, {
+                i(1, clipboard)
+            })
+        end, {}),
         t(")"),
     }),
 
     s("mathblock", {
         t({ "$$", "" }),
         i(1, "maths"),
-        t({ "", "$$","" }),
+        t({ "", "$$", "" }),
     }),
 
     s("mathinline", {
