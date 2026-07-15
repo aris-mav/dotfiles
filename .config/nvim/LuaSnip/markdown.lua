ab27375ea4d11ff2a1a1bc8b9af48fe892f3953b
arismav <aris.mav@hotmail.com>
2026-07-15 11:38:11 +0200

    rm yaml template

--- .config/nvim/LuaSnip/markdown.lua
+++ .config/nvim/LuaSnip/markdown.lua
@@ -5,42 +5,6 @@ local i = ls.insert_node
 local d = ls.dynamic_node
 local sn = ls.snippet_node
 
-local yaml_header = s({},
-    {
-        t({ "---", "date: " }),
-        f(function() return os.date("%Y-%m-%d") end, {}),
-        t({ "", 'title: "' }), i(1),
-        t({ '"', 'source: "' }), i(2),
-        t({ '"', "tags:", "  - " }), i(3),
-        t({ "", "---", "", "" }),
-        i(0)
-    }
-)
-
-local function is_nt()
-    local basename = vim.fn.expand("%:t:r")
-    return #basename == 14 and basename:match("^%d+$") ~= nil
-end
-
-local function is_new_file()
-    return vim.fn.filereadable(vim.fn.expand("%:p")) == 0
-end
-
-local function expand_auto_header()
-    if not is_nt() or not is_new_file() then
-        return
-    end
-    vim.schedule(function()
-        ls.snip_expand(yaml_header)
-        vim.cmd("startinsert!")
-    end)
-end
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "markdown",
-    callback = expand_auto_header,
-})
-
 return {
     s("link", {
         t("["),
