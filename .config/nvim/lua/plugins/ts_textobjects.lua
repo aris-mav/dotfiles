7fbeaf07ce7768861fd908114157bd75e7ce4cee
arismav <aris.mav@hotmail.com>
2026-07-17 15:31:56 +0200

    remove unused `assignment` ts_textobjects

--- .config/nvim/lua/plugins/ts_textobjects.lua
+++ .config/nvim/lua/plugins/ts_textobjects.lua
@@ -22,10 +22,6 @@ return {
         local move = require("nvim-treesitter-textobjects.move")
 
         local select_keymaps = {
-            ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-            ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
             ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
             ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
             ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
