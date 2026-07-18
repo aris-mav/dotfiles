da8ff9fa56fd728726936b45207be739e8d86030
arismav <aris.mav@hotmail.com>
2026-07-18 23:02:55 +0200

    rearrange

--- .config/nvim/lua/plugins/lspPlugins.lua
+++ .config/nvim/lua/plugins/lspPlugins.lua
@@ -1,31 +1,28 @@
 return {
     {
         'williamboman/mason.nvim',
-        event = "VeryLazy",
+        event = 'VeryLazy',
         opts = {},
-        dependencies = {
-            { 'williamboman/mason-lspconfig.nvim' },
-        },
     },
-
     {
         'neovim/nvim-lspconfig',
         cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
         event = { 'BufReadPre', 'BufNewFile' },
-        config = function()
-            local lsp_defaults = require('lspconfig').util.default_config
-
-            require('mason-lspconfig').setup({
-                ensure_installed = {},
-                handlers = {
-                    function(server_name)
-                        vim.lsp.enable(server_name)
-                    end,
-                }
-            })
-
-            -- enable fish lsp
-            vim.lsp.enable('fish_lsp')
-        end
-    }
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'neovim/nvim-lspconfig',
+        },
+        opts = {
+            ensure_installed = {
+                'fish_lsp',
+                'markdown_oxide',
+                'ruff', 'ty',
+                'lua_ls',
+                'bashls',
+            },
+        },
+    },
 }
