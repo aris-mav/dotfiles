8928fab02d1437a63056577b58dd41a7c191cba4
arismav <aris.mav@hotmail.com>
2026-07-18 11:22:50 +0200

    add nt completions

---
+++ .config/fish/completions/nt.fish
@@ -0,0 +1,18 @@
+# allows multiple tag completions
+function __nt_using_s
+    set -l tokens (commandline -opc)
+    contains -- -s $tokens
+end
+
+complete -c nt -s s -fra '(nt -t)' -d 'tag'
+complete -c nt -n __nt_using_s -fra '(nt -t)' -d 'tag'
+complete -c nt -d 'edit file' -fra '(ls $NOTES_DIR)'
+
+complete -c nt -s s -d 'content search' -fr
+complete -c nt -s n -d 'creante new note' -f
+complete -c nt -s S -d 'list all sources' -f
+complete -c nt -s H -d 'list all headers' -f
+complete -c nt -s t -d 'list all tags' -f
+complete -c nt -s T -d 'open TODO.txt' -f
+complete -c nt -s b -d 'log finished book' -f
+complete -c nt -s p -d 'html preview' -fra '(ls $NOTES_DIR)'
