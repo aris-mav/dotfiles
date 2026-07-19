01901381902495e05a0c7d9e18037341c642538c
arismav <aris.mav@hotmail.com>
2026-07-19 09:14:25 +0200

    rm autoinstalls, buggy on termux

--- .config/nvim/lua/plugins/lspPlugins.lua
+++ .config/nvim/lua/plugins/lspPlugins.lua
@@ -15,14 +15,6 @@ return {
             'williamboman/mason.nvim',
             'neovim/nvim-lspconfig',
         },
-        opts = {
-            ensure_installed = {
-                'fish_lsp',
-                'markdown_oxide',
-                'ruff', 'ty',
-                'lua_ls',
-                'bashls',
-            },
-        },
+        opts = {},
     },
 }
