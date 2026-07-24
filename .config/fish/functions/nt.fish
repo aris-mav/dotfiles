2086ccfd37645d723e8f32442b07d8f81aff9e6a
arismav <aris.mav@hotmail.com>
2026-07-24 20:43:19 +0200

    update no argument option

--- .config/fish/functions/nt.fish
+++ .config/fish/functions/nt.fish
@@ -1,10 +1,13 @@
 function nt
 
-    if test (count $argv) -eq 0
-        $NOTES_DIR/nt.sh no_args
-        cd $NOTES_DIR
-    else 
-        $NOTES_DIR/nt.sh $argv
-    end    
+    if set -q NOTES_DIR
+        if test (count $argv) -eq 0
+            cd $NOTES_DIR
+        else
+            $NOTES_DIR/nt.sh $argv
+        end
+    else
+        echo "NOTES_DIR is not set."
+    end
 
 end
