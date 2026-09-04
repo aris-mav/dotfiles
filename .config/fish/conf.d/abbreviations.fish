e2cd73d0f211b318e17404d7f45ccf064eb23bd8
arismav <aris.mav@hotmail.com>
2026-09-04 22:54:09 +0200

    add -m on gc

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,53 +1,52 @@
-    abbr gc 'git commit'
-    abbr gC 'git checkout'
-    abbr gr 'git restore'
-    abbr gR 'git reset'
-    abbr gf 'git fetch'
-    abbr ga 'git add'
-    abbr gd 'git diff'
-    abbr gs 'git status'
-    abbr gS 'git stash'
-    abbr gp 'git pull'
-    abbr gP 'git push'
-    abbr gb 'git branch'
-    abbr gl 'git log'
-    abbr gm 'git merge'
-    abbr grb 'git rebase'
-    abbr gcp 'git cherry-pick'
-    abbr gau 'git add -u'
-    abbr gac 'git commit -a'
-    abbr gacp 'git commit -a ; git push'
+abbr --set-cursor=% gc "git commit -m '%'"
+abbr gC 'git checkout'
+abbr gr 'git restore'
+abbr gR 'git reset'
+abbr gf 'git fetch'
+abbr ga 'git add'
+abbr gd 'git diff'
+abbr gs 'git status'
+abbr gS 'git stash'
+abbr gp 'git pull'
+abbr gP 'git push'
+abbr gb 'git branch'
+abbr gl 'git log'
+abbr gm 'git merge'
+abbr grb 'git rebase'
+abbr gcp 'git cherry-pick'
+abbr gau 'git add -u'
+abbr gac 'git commit -a'
+abbr gacp 'git commit -a ; git push'
 
-    if set -q FUZZYFIND
+if set -q FUZZYFIND
 
-        abbr gaf "git add (git diff --name-only \
+    abbr gaf "git add (git diff --name-only \
         | $FUZZYFIND --multi --preview 'git diff --color=always {}')"
 
-        abbr grf "git reset (git diff --name-only --cached \
+    abbr grf "git reset (git diff --name-only --cached \
         | $FUZZYFIND --multi --preview 'git diff --color=always --cached {}')"
 
-        abbr gdf "git diff --name-only \
+    abbr gdf "git diff --name-only \
         | $FUZZYFIND --multi --preview 'git diff --color=always {}'"
 
-        abbr cdp "cd (tmux list-panes -a -F '#{pane_current_path}' | $FUZZYFIND)"
+    abbr cdp "cd (tmux list-panes -a -F '#{pane_current_path}' | $FUZZYFIND)"
 
-    end
+end
 
-    abbr e "$EDITOR"
-    abbr f "$FILE_BROWSER"
-    abbr y fish_clipboard_copy
-    abbr xo 'xdg-open'
-    abbr x 'xargs'
+abbr e "$EDITOR"
+abbr f "$FILE_BROWSER"
+abbr y fish_clipboard_copy
+abbr xo xdg-open
+abbr x xargs
 
-    abbr --set-cursor=% n "nt -%" 
+abbr --set-cursor=% n "nt -%"
 
-    if type -q pacman
-        abbr pa 'sudo pacman'
-    else if type -q apt
-        abbr pa 'sudo apt'
-    else if type -q nix
-        abbr rb 'sudo nixos-rebuild switch \
+if type -q pacman
+    abbr pa 'sudo pacman'
+else if type -q apt
+    abbr pa 'sudo apt'
+else if type -q nix
+    abbr rb 'sudo nixos-rebuild switch \
         -I nixos-config=~/.config/nixos/configuration.nix'
-        abbr ns 'nix-shell --command fish -p '
-    end
-
+    abbr ns 'nix-shell --command fish -p '
+end
